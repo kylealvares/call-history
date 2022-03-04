71522e21a8c99b38e2442c938e46d53bbe41c8de
kyle <kyle.alv2000@gmail.com>
2022-03-04 15:56:09 -0500

    profanity checks

--- app.js
+++ app.js
@@ -1,48 +1,55 @@
 require('dotenv').config();
+
+const sadGifs = require('./sad-gifs.json');
+
 const express = require('express');
 const Discord = require("discord.js");
+const WordFilter = require('bad-words');
 
 const app = express();
+const wordFilter = new WordFilter();
 
 app.listen(3000, () => {
-  console.log('Join the Call bot is running!')
+    console.log('Join the Call bot is running!')
 });
 
 app.get("/", (req, res) => {
-  res.send("Join the Call Discord Bot");
+    res.send("Call History Discord Bot running...");
 });
 
-
-const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"]});
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"] });
 
 const channelID = process.env.channelID;
 
 client.on("messageCreate", message => {
-  if(message.content === "jtc") {
-    message.channel.send("Lets you know when users join and leave the call");
-  }
+    if (message.content === "jtc") {
+        message.channel.send("Lets you know when users join and leave the call");
+    } else if (wordFilter.isProfane(message.content)) {
+        message.channel.send(sadGifs.gifs[Math.floor(Math.random() * sadGifs.gifs.length)])
+    }
 })
 
-client.on("voiceStateUpdate", async (oldState, newState) => { 
+client.on("voiceStateUpdate", async (oldState, newState) => {
 
-  if(oldState.channel !== null && newState.channel !== null)   return;
-  
-  const userID = oldState.id;
+    if (oldState.channel !== null && newState.channel !== null) return;
 
-  const Guild = client.guilds.cache.get(channelID);
-  const Member = Guild.members.cache.get(userID);
-  
-  const user = await client.users.cache.find(user => user.id === userID);
-  const username = user.username;
-  const channel = await client.channels.cache.find(channel => channel.name === "call-history");
-  
-  if(Member.voice.channel) {
-    channel.send(`<@${userID}> joined the call.`);
-  } else {
-    channel.send(`<@${userID}> has left the call.`);
-  }
+    const userID = oldState.id;
 
-  
+    const Guild = client.guilds.cache.get(channelID);
+    const Member = Guild.members.cache.get(userID);
+
+    const user = await client.users.cache.find(user => user.id === userID);
+    const username = user.username;
+    const channel = await client.channels.cache.find(channel => channel.name === "call-history");
+
+
+    if (Member.voice.channel) {
+        channel.send(`<@${userID}> joined the call in ${newState.channel}.`);
+    } else {
+        channel.send(`<@${userID}> has left the call in ${oldState.channel}.`);
+    }
+
+
 });
 
 client.login(process.env.token);
