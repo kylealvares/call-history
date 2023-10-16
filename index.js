9b6334aa8cb3d2105e7d6d70daa2d4c37137b360
kylealvares <kyle.alv2000@gmail.com>
2023-10-15 22:56:03 -0400

    vanish mode

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 
-const sadGifs = require('./sad-gifs.json');
 const Discord = require("discord.js");
 const WordFilter = require('bad-words');
 const express = require('express');
@@ -21,18 +20,45 @@ const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD
 
 const logsChannelID = process.env.LOGS_CHANNEL_ID;
 const activeChannelID = process.env.ACTIVE_CHANNEL_ID;
+const vibesChannelID = process.env.VIBES_CHANNEL_ID;
 
 client.on("messageCreate", message => {
+
+    if (message.channelId === vibesChannelID) {
+        if (message.content === "purge") {
+            message.channel.messages.fetch().then(messages => {
+                message.channel.bulkDelete(messages.filter(msg => msg.author.id === message.author.id));
+            })
+        } else if (message.content === "sudo purge") {
+            message.channel.messages.fetch().then(messages => {
+                message.channel.bulkDelete(messages);
+            })
+        } else if (message.content === "vibes") {
+            if (message.content.toLowerCase().includes('vibes')) {
+                const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_API_KEY}&tag=vibes&rating=g`;
+                fetch(url)
+                    .then(res => res.json())
+                    .then(json => message.channel.send(json.data.url))
+                    .catch(err => console.error(err));
+            }
+        }
+        return;
+    } 
+
     if (message.content === "jtc") {
         message.channel.send("Lets you know when users join and leave the call");
     } else if (wordFilter.isProfane(message.content)) {
-        message.channel.send(sadGifs.gifs[Math.floor(Math.random() * sadGifs.gifs.length)])
+        const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_API_KEY}&tag=sad&rating=g`;
+        fetch(url)
+            .then(res => res.json())
+            .then(json => message.channel.send(json.data.url))
+            .catch(err => console.error(err));
     }
-})
+});
 
 client.on("voiceStateUpdate", async (oldState, newState) => {
 
-      // check if the user has joined or left a voice channel
+    // check if the user has joined or left a voice channel
     if (oldState.channel !== null && newState.channel !== null) return;
 
     const userID = oldState.id;
@@ -63,7 +89,7 @@ client.on("voiceStateUpdate", async (oldState, newState) => {
         activeChannel.bulkDelete(messages);
     });
 
-      // send a message to the active members channel
+    // send a message to the active members channel
     if (members.length === 0) {
         onCallChannel.send(`https://tenor.com/view/pettyratz-call-me-bored-sad-gif-22155447`);
     } else {
