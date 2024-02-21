44ad9fbd822957d7c5fdd7a2c9a4d12f717fca9d
Arshdeep Singh Saini <17682945+Omegashin@users.noreply.github.com>
2024-02-20 19:03:05 -0500

    docs: slash commands ✨

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,5 +1,9 @@
 import { SlashCommandBuilder } from "discord.js";
 
+// https://discord.com/developers/docs/interactions/application-commands
+// use deploy-commands.js to redeploy the command if you need to update the name or description
+// the execute() function doesn't need to be redeployed
+
 export const ping = {
   data: new SlashCommandBuilder()
     .setName("ping")
