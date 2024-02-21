9def8692ed4ef5d16741707bb066f1b06591fa7d
Arshdeep Singh Saini <17682945+Omegashin@users.noreply.github.com>
2024-02-20 20:43:21 -0500

    style: modular events 💎

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// imports
-import axios from "axios";
 import WordFilter from "bad-words";
 import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
@@ -7,17 +5,12 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import express from "express";
-import cron from "node-cron";
-import { formattedQuote } from "./utils.js";
 
 dotenv.config();
 
 // constants
-const logsChannelId = process.env.LOGS_CHANNEL_ID;
-const activeChannelId = process.env.ACTIVE_CHANNEL_ID;
 const vibesChannelId = process.env.VIBES_CHANNEL_ID;
 const quotesChannelId = process.env.QUOTES_CHANNEL_ID;
-const voiceChannelId = process.env.VOICE_CHANNEL_ID;
 
 // server
 const app = express();
@@ -46,7 +39,7 @@ const client = new Client({
   ],
 });
 
-// slash commands
+// read slash commands
 
 client.commands = new Collection();
 
@@ -61,12 +54,13 @@ for (const folder of commandFolders) {
     .filter((file) => file.endsWith(".js"));
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
-    let command;
-    if (process.platform === "win32") {
-      command = await import(`file://${filePath.replace(/\\/g, "/")}`);
-    } else {
-      command = await import(filePath);
-    }
+
+    let command = await import(
+      process.platform === "win32"
+        ? `file://${filePath.replace(/\\/g, "/")}`
+        : filePath
+    );
+
     command = command[file.split(".")[0]];
     // Set a new item in the Collection with the key as the command name and the value as the exported module
     if ("data" in command && "execute" in command) {
@@ -79,63 +73,32 @@ for (const folder of commandFolders) {
   }
 }
 
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
+// read events
 
-  const command = interaction.client.commands.get(interaction.commandName);
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
 
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+
+  const event = await import(
+    process.platform === "win32"
+      ? `file://${filePath.replace(/\\/g, "/")}`
+      : filePath
+  );
+
+  if (event.default.once) {
+    client.once(event.default.name, (...args) =>
+      event.default.execute(...args)
+    );
+  } else {
+    client.on(event.default.name, (...args) => event.default.execute(...args));
   }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    } else {
-      await interaction.reply({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    }
-  }
-});
+}
 
 // events
-
-// bot wakes up
-client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.username}`);
-
-  cron.schedule("30 9 * * *", () => {
-    const quotesChannel = client.channels.cache.get(quotesChannelId);
-    if (quotesChannel) {
-      axios.get("https://zenquotes.io/api/today").then((data) => {
-        quotesChannel.send(formattedQuote(data));
-      });
-    } else {
-      console.log("Quotes channel not found");
-    }
-  });
-
-  cron.schedule("0 5 7,21 * *", () => {
-    const vibesChannel = client.channels.cache.get(vibesChannelId);
-    if (vibesChannel) {
-      vibesChannel.messages
-        .fetch()
-        .then((messages) => vibesChannel.bulkDelete(messages))
-        .catch((err) => console.log("Bi-weekly purging:", err));
-    } else {
-      console.log("Vibes channel not found");
-    }
-  });
-});
 
 // vanish mode (in 😇vibes)
 client.on(Events.MessageCreate, async (message) => {
@@ -221,45 +184,6 @@ client.on(Events.MessageCreate, async (message) => {
       }
       return;
     }
-  }
-});
-
-client.on(Events.VoiceStateUpdate, async (oldState, newState) => {
-  // check if the user has joined or left a voice channel
-  if (oldState.channel !== null && newState.channel !== null) return;
-
-  const userId = oldState.id;
-
-  const Guild = client.guilds.cache.get(oldState.guild.id);
-  const Member = Guild.members.cache.get(userId);
-
-  const user = await client.users.cache.find((user) => user.id === userId);
-  const logsChannel = client.channels.cache.get(logsChannelId);
-
-  // send a message to the log channel indicating if the user joined or left the call
-  logsChannel.send(
-    `${user.username} ${Member.voice.channel ? "joined" : "has left"} the call.`
-  );
-
-  // get the active members in the voice channel
-  const channel = client.channels.cache.get(voiceChannelId);
-  const members = channel.members.map((member) => member.displayName);
-
-  // delete previous messages from active channel
-  const activeChannel = client.channels.cache.get(activeChannelId);
-  activeChannel.messages.fetch().then((messages) => {
-    activeChannel.bulkDelete(messages);
-  });
-
-  // send a message to the active members channel
-  if (members.length === 0) {
-    activeChannel.send(
-      "https://tenor.com/view/pettyratz-call-me-bored-sad-gif-22155447"
-    );
-  } else {
-    activeChannel.send(
-      `${channel.name}: ${members.map((member) => member).join(", ")}.`
-    );
   }
 });
 
